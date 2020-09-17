6b646345b41ef2903c29c191b4f601f4564504c0
magical-builder-5600 <gabbinguyen@gmail.com>
2020-09-17 01:20:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   return Object.assign({}, object, { [key] });
-  delete object[key];
+  delete object.key;
   return object;
 }
 
